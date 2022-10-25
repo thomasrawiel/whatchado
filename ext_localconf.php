2171e67ec3be1f8f7dc6946375ef547a2dbbef35
thomasrawiel <thomasrawiel@users.noreply.github.com>
2022-10-25 11:23:14 +0200

    bugfix php 8

--- ext_localconf.php
+++ ext_localconf.php
@@ -1,10 +1,9 @@
 <?php
 defined('TYPO3') or die('Access denied.');
-call_user_func(function ($_EXTKEY) {
+call_user_func(function () {
     $GLOBALS['TYPO3_CONF_VARS']['SYS']['fal']['onlineMediaHelpers']['whatchado']
         = \TRAW\Whatchado\Helpers\WhatchadoHelper::class;
-
-    $rendererRegistry = \TYPO3\CMS\Core\Resource\Rendering\RendererRegistry::getInstance();
+    $rendererRegistry = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(\TYPO3\CMS\Core\Resource\Rendering\RendererRegistry::class);
     $rendererRegistry->registerRendererClass(
         \TRAW\Whatchado\Resource\Rendering\WhatchadoRenderer::class
     );
@@ -14,4 +13,4 @@ call_user_func(function ($_EXTKEY) {
 
     // Register custom file extension as allowed media file
     $GLOBALS['TYPO3_CONF_VARS']['SYS']['mediafile_ext'] .= ',whatchado';
-}, $_EXTKEY);
+});
